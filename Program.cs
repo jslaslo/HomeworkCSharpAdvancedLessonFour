b199893a8b22ab1d7be16dfe0c8a41b423e1d47d
dergousov.vladislav <125125092+jslaslo@users.noreply.github.com>
2023-12-05 15:13:57 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using static Homework.TaskOne;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bits bit = new(100);
+            Bits bitInt = new(99);
+            Bits bitByte = new(50);
+            Bits bitLong = new(3);
+
+            Console.WriteLine(bit);
+            Console.WriteLine(bitInt);
+            Console.WriteLine(bitByte);
+            Console.WriteLine(bitLong);
+
+        }
+    }
+}
